d4dedf31d163f5904f4b09292442080b6d577c4a
Jan Rosell <jan.rosell@upc.edu>
2023-03-22 14:10:14 +0100

    my changes

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Source Gazebo environment
-source /usr/local/share/gazebo-9/setup.sh
+#source /usr/local/share/gazebo-9/setup.sh
+source /usr/share/gazebo/setup.sh
 
 # Export location to GAP 1.5 plugins
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/usr/local/lib/gap-1.5
+#export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/usr/local/lib/gap-1.5
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:~/catkin/catkin_wsGrasp/install/lib/gap-1.5
+
 # Export GAP message library
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/gap-1.5
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/gap-1.5
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/catkin/catkin_wsGrasp/install/lib/gap-1.5
 
 # Export location of grasp plugins
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:`pwd`/build/lib
